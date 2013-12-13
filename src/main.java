eb98d019b144560d63ff4b26a8024f3b6f156533
JuanMNGA <juanmiguel.ruiz.ladron@outlook.com>
2013-12-13 13:52:39 +0100

    Añadidos nuevos botones e indicadores

--- src/main.java
+++ src/main.java
@@ -9,12 +9,20 @@ import org.jsfml.system.*;
 public class main {
 	public static void main(String[] args) throws Exception{
 		RenderWindow Window = new RenderWindow(new VideoMode(600,500,32),"Role & Music");
+		//Variables
+		boolean Set_Rep=false;
+		boolean[] Sel_Lab = new boolean[7];
+		//Inicializacion de variables
+		for(int i=0;i<7;i++)
+			Sel_Lab[i]=false;
 		//Creacion de objetos
-		Texture[] TX = new Texture[4];
+		Texture[] TX = new Texture[7];
 		Sprite BG;
 		Sprite[] Play = new Sprite[7];
 		Sprite[] Stop = new Sprite[7];
 		Sprite[] Label = new Sprite[7];
+		Sprite[] Repr = new Sprite[2];
+		Sprite[] SelLab = new Sprite[7];
 		Text[] Textos = new Text[2];
 		Font fuente = new Font();
 		SoundBuffer[] PistasBuffer = new SoundBuffer[7];
@@ -29,29 +37,46 @@ public class main {
 		Textos[1] = new Text(" Cliquee encima del espacio para escritura, cuando se marque el recuadro,\n escriba el nombre de la cancion deseada acabado en .wav o .ogg,\n la cancion debe estar en formato .wav o .ogg para su correcta\n reproduccion. Por ultimo, pulse enter para cargar la cancion.",fuente,20);
 		Textos[1].setColor(Color.RED);
 		//Inicializacion de texturas y sprites
-		for(int i=0;i<4;i++)
+		for(int i=0;i<7;i++)
 			TX[i]= new Texture();
-
+			//Fondo
 		TX[0].loadFromFile(Paths.get("Images/Background.bmp"));
 		BG = new Sprite(TX[0]);
+			//Sprites botones Play
 		TX[1].loadFromFile(Paths.get("Images/Play.png"));
 		for(int i=0;i<7;i++){
 			Play[i] = new Sprite(TX[1]);
 			Play[i].setPosition(400,150+(45*i));
 		}
+			//Sprites botones Stop
 		TX[2].loadFromFile(Paths.get("Images/Stop.png"));
 		for(int i=0;i<7;i++){
 			Stop[i] = new Sprite(TX[2]);
 			Stop[i].setPosition(455,150+(45*i));
 		}
+			//Sprites botones Labels
 		TX[3].loadFromFile(Paths.get("Images/Label.png"));
 		for(int i=0;i<7;i++){
 			Label[i] = new Sprite(TX[3]);
-			Label[i].setPosition(45,150+(45*i));
+			Label[i].setPosition(60,150+(45*i));
+		}
+			//Sprites botones reproductor
+		TX[4].loadFromFile(Paths.get("Images/ReproductorG.png"));
+		Repr[0] = new Sprite(TX[4]);
+		Repr[0].setPosition(400,90);
+		TX[5].loadFromFile(Paths.get("Images/ReproductorB.png"));
+		Repr[1] = new Sprite(TX[5]);
+		Repr[1].setPosition(400,90);
+			//Sprites seleccion label
+		TX[6].loadFromFile(Paths.get("Images/Select.png"));
+		for(int i=0;i<7;i++){
+			SelLab[i] = new Sprite(TX[6]);
+			SelLab[i].setPosition(10,150+(45*i));
 		}
 		//Inicializacion de pistas
 		for(int i=0;i<7;i++)
 			PistasBuffer[i] = new SoundBuffer();
+			//Carga de archivos de audio (inclusion a pelo, hay que cambiarlo a por entrada de texto)
 		PistasBuffer[0].loadFromFile(Paths.get("Samples/1.wav"));
 		Pistas[0] = new Sound(PistasBuffer[0]);
 		PistasBuffer[1].loadFromFile(Paths.get("Samples/2.wav"));
@@ -69,33 +94,61 @@ public class main {
 						//Click Izquierdo del raton
 						if(event.asMouseButtonEvent().button == Mouse.Button.LEFT){
 							Vector2f Position = new Vector2f(Mouse.getPosition(Window));
-							for(int i=0;i<7;i++){
-								if(Play[i].getGlobalBounds().contains(Position.x,Position.y)){
-									Pistas[i].play();
+							if(Position.x>=400){
+								for(int i=0;i<7;i++){
+									if(Play[i].getGlobalBounds().contains(Position.x,Position.y)){
+										Pistas[i].play();
+									}
+									if(Stop[i].getGlobalBounds().contains(Position.x,Position.y)){
+										Pistas[i].stop();
+									}
 								}
-								if(Stop[i].getGlobalBounds().contains(Position.x,Position.y)){
-									Pistas[i].stop();
+							}//Fin if posicion
+							if(Repr[0].getGlobalBounds().contains(Position.x,Position.y)){
+								if(Set_Rep==false){
+									Set_Rep=true;
+								}else{
+									Set_Rep=false;
+								}
+							}//Fin if Modo reproductor
+							if(Position.x>60 && Position.x<315){
+								for(int i=0;i<7;i++){
+									if(Label[i].getGlobalBounds().contains(Position.x,Position.y)){
+										Sel_Lab[i] = true;
+									}else{
+										Sel_Lab[i] = false;
+									}
 								}
 							}
 						}
 						//Click Derecho del raton
 						if(event.asMouseButtonEvent().button == Mouse.Button.RIGHT){
 							Vector2f Position = new Vector2f(Mouse.getPosition(Window));
-							for(int i=0;i<7;i++){
-								if(Play[i].getGlobalBounds().contains(Position.x,Position.y)){
-									Pistas[i].setLoop(true);
-									Pistas[i].play();
+							if(Position.x>=400){
+								for(int i=0;i<7;i++){
+									if(Play[i].getGlobalBounds().contains(Position.x,Position.y)){
+										Pistas[i].setLoop(true);
+										Pistas[i].play();
+									}
 								}
-							}
+							}//Fin if posicion
 						}
-					}
-				}
+					}//Fin if botones
+				}//Fin if evento de raton
 			}
 			Window.clear();
 			for(int i=0;i<7;i++){
 				Window.draw(Play[i]);
 				Window.draw(Stop[i]);
 				Window.draw(Label[i]);
+			}
+			if(Set_Rep==true){
+				Window.draw(Repr[0]);
+			}else{
+				Window.draw(Repr[1]);
+			}
+			for(int i=0;i<7;i++){
+				if(Sel_Lab[i]==true)Window.draw(SelLab[i]);
 			}
 			Window.draw(Textos[0]);
 			Window.draw(Textos[1]);
